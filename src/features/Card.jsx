78c50f2b819a9d0e3fb46d347f11d554b79ee6d6
Nnamdi Echem <nnamdi.b@gmail.com>
2025-10-22 00:19:43 +0200

    fixed: bug in dataset, added paragraphs to table, added a view for the card, syled the card

--- src/features/Card.jsx
+++ src/features/Card.jsx
@@ -8,23 +8,39 @@ function Card(props) {
   }
   return (
     <div>
-      <h3>{pokemon.name.english}</h3>
+      <h3>
+        {pokemon.name.english} #{pokemon.id}
+      </h3>
       <img src={`/images/${getFormattedFileName(pokemon.id)}`} alt="Pokemon" />
       <p>
         Type: {pokemon.type[0]} {pokemon.type[1]}
       </p>
-      <tbody>
-        <tr>
-          <td>Attack: {pokemon.base.Attack}</td>
-          <td>Defense: {pokemon.base.Defense}</td>
-          <td>HP: {pokemon.base.HP}</td>
-        </tr>
-        <tr>
-          <td>Sp. Attack: {pokemon.base["Sp. Attack"]}</td>
-          <td>Sp. Defense: {pokemon.base["Sp. Defense"]}</td>
-          <td>Speed: {pokemon.base.Speed}</td>
-        </tr>
-      </tbody>
+      <div className="pokemon-view">
+        <tbody>
+          <tr>
+            <td>
+              <p>Attack: {pokemon.base.Attack}</p>
+            </td>
+            <td>
+              <p>Defense: {pokemon.base.Defense}</p>
+            </td>
+            <td>
+              <p>HP: {pokemon.base.HP}</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p>Sp. Attack: {pokemon.base["Sp. Attack"]}</p>
+            </td>
+            <td>
+              <p>Sp. Defense: {pokemon.base["Sp. Defense"]}</p>
+            </td>
+            <td>
+              <p>Speed: {pokemon.base.Speed}</p>
+            </td>
+          </tr>
+        </tbody>
+      </div>
     </div>
   );
 }
