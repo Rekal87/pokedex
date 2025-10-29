68db92e64c10127a104db495886a3a2255ae7635
Nnamdi Echem <nnamdi.b@gmail.com>
2025-10-29 19:16:40 +0100

    created component

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,10 @@ function App() {
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="mx-auto max-w-2xl text-center">
           <h1 className="text-5xl font-semibold tracking-tight text-balance text-white sm:text-7xl">
-            hello world
+            Pokedéx
           </h1>
+          <div className="menu"></div>
+          <section></section>
         </div>
       </div>
     </div>
